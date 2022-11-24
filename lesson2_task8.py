3545c6a5548bf9a4822fc89f911f9034439ed30d
PavelSB <1987PavelS@mail.ru>
2022-11-24 21:20:04 +0300

    lesson 2 task 8 done

---
+++ lesson2_task8.py
@@ -0,0 +1,10 @@
+# 6. Вводится строка. С помощью метода String.find найдите в этой строке
+# индекс первого вхождения фрагмента "ra". Полученное число выведите на экран.
+
+# Sample Input:
+# abrakadabra
+# Sample Output:
+# 2
+
+str = input("Enter string: ")
+print(str.find('ra'))
