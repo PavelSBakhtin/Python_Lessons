bcb764c4fdb7a6aef94e99e46a47be53eadf7979
PavelSB <1987PavelS@mail.ru>
2022-11-30 21:25:13 +0300

    lesson 2 task 6 done

--- lesson2_task6.py
+++ lesson2_task6.py
@@ -1,14 +1,15 @@
-# 2. Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
-# *Пример:*
-# Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+# Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
+# Пример: Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n = int(input("Enter an integer N: "))
-dc = {}
-for i in range(n):
-    dc[i + 1] = 3 * (i + 1) + 1
+dc = {i: 3 * i + 1 for i in range(1, n + 1)}
+## Или так:
+# dc = {}
+# for i in range(n):
+#     dc[i + 1] = 3 * (i + 1) + 1
 print(dc)
 
-# d = {'key': 'value'}
-# print(d)  # {'key': 'value'}
-# d['mynewkey'] = 'mynewvalue'
-# print(d)  # {'key': 'value', 'mynewkey': 'mynewvalue'}
+# dc = {'key': 'value'}
+# print(dc)  # {'key': 'value'}
+# dc['mynewkey'] = 'mynewvalue'
+# print(dc)  # {'key': 'value', 'mynewkey': 'mynewvalue'}
