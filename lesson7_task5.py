35a84a04dacca15c449d906c4b1f7b35c5a337dd
PavelSB <1987PavelS@mail.ru>
2022-12-13 20:20:29 +0300

    lesson 7 task 5 done

---
+++ lesson7_task5.py
@@ -0,0 +1,17 @@
+# Напишите функцию triangle(a, b, c), которая принимает на вход три длины отрезков
+# и определяет, можно ли из этих отрезков составить треугольник.
+# Выходные данные
+# triangle(1, 1, 2)
+# Это не треугольник
+# triangle(7, 6, 10)
+
+def Triangle(a, b, c):
+    if a+b > c and a+c > b and b+c > a:
+        return True
+    else:
+        return False
+
+a = int(input('введите а: '))
+b = int(input('введите b: '))
+c = int(input('введите c: '))
+print('Это треугольник' if Triangle(a, b, c) else "Это не треугольник")
